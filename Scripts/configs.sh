1a4abec347d967f987f02ab50fffb345c31daf94
NGxID18 <nigelginting123@gmail.com>
2026-06-08 01:57:22 +0700

    tes

--- Scripts/configs.sh
+++ Scripts/configs.sh
@@ -16,11 +16,4 @@ ln -sf "$DOTFILES_DIR/.config/ghostty/config.ghostty" ~/.config/ghostty/config
 echo "Applying GNOME Extensions configuration using dconf..."
 dconf load /org/gnome/shell/extensions/ < "$DOTFILES_DIR/.config/dconf/gnome-extensions.dconf"
 
-echo "Applying SDDM configuration..."
-sudo mkdir -p /usr/share/sddm/themes
-sudo mkdir -p /etc/sddm.conf.d
-
-sudo cp -r "$DOTFILES_DIR/System/sddm/themes/Sugar-Candy" /usr/share/sddm/themes/
-sudo cp "$DOTFILES_DIR/System/sddm/sddm.conf" /etc/sddm.conf.d/custom.conf
-
 echo "Configuration applied successfully!"
