086e200972e629d39ee1aed4e93185a901e0dc3e
Fitznigel Ginting <nigelginting123@gmail.com>
2026-06-11 19:08:39 +0700

    test

--- Scripts/dependencies.sh
+++ Scripts/dependencies.sh
@@ -44,11 +44,9 @@ paru -S --noconfirm \
 # Flatpak
 echo "Installing Flatpak and the GNOME Software plugin for Flatpak..."
 sudo pacman -S flatpak --noconfirm \
-    gnome-software-plugin-flatpak \
     flatseal
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub -y
 
 echo "Installing applications from Flathub using Flatpak..."
 flatpak install -y \
